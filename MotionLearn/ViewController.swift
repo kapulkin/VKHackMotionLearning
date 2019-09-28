03042a395e428c4b5a5eb711b070b88fff15db62
David <amahstla@yandex.ru>
2019-09-28 16:25:12 +0300

    add video support

--- MotionLearn/ViewController.swift
+++ MotionLearn/ViewController.swift
@@ -9,16 +9,42 @@
 import UIKit
 import SceneKit
 import ARKit
+import ReplayKit
+//import PhotosUI
+//import Photos
 
-class ViewController: UIViewController, ARSCNViewDelegate {
+class ViewController: UIViewController, ARSCNViewDelegate, RecordButtonDelegate, RPScreenRecorderDelegate {
+  
+  @IBOutlet var button: RecordButton!
+  @IBOutlet var sceneView: ARSCNView!
+  var recorder: RPScreenRecorder!
 
-    @IBOutlet var sceneView: ARSCNView!
-    
     var img: UIImage! = UIImage(named: "mona-lisa")
+  func tapButton(isRecording: Bool) {
+
+    if !isRecording {
+      recorder.stopRecording { (preview, error) in
+          if let unwrappedPreview = preview {
+              unwrappedPreview.previewControllerDelegate = self
+              self.present(unwrappedPreview, animated: true)
+          }
+      }
+        print("Start recording")
+    } else {
+        print("Stop recording")
+      recorder.startRecording { error in
+            if let unwrappedError = error {
+                print(unwrappedError.localizedDescription)
+            } else {
+            }
+        }
+    }
+  }
   
     override func viewDidLoad() {
         super.viewDidLoad()
-
+//      iv.image = img
+      button.delegate = self
                 // Create a session configuration
                 let configuration = ARWorldTrackingConfiguration()
         //        configuration.planeDetection = .vertical
@@ -58,26 +84,104 @@ class ViewController: UIViewController, ARSCNViewDelegate {
       
       let ballNode = SCNNode(geometry: ball)
       ballNode.geometry?.firstMaterial?.diffuse.contents = img
-//      let fov = sceneView.pointOfView?.camera?.fieldOfView ?? 0.0
-//      print("fov = \(fov)")
-//      let a: CGFloat = CGFloat(tanf((GLKMathDegreesToRadians(Float(fov/2.0)))))
-//      let z = ((size.height / 2.0) / a)
-        
       ballNode.position = SCNVector3Make(0, 0, -0.2)
-      sceneView.pointOfView?.addChildNode(ballNode)
       
-//      let button = UIButton()
-//      button.translatesAutoresizingMaskIntoConstraints = false
-//
-//      self.view.addSubview(button)
-        
+      recorder = RPScreenRecorder.shared()
+      recorder.delegate = self
+      
+      // A SpriteKit scene to contain the SpriteKit video node
+      let spriteKitScene = SKScene(size: CGSize(width: sceneView.frame.width, height: sceneView.frame.height))
+      spriteKitScene.scaleMode = .aspectFit
+
+      // Create a video player, which will be responsible for the playback of the video material
+      let videoUrl = Bundle.main.url(forResource: "video", withExtension: "mp4")!
+      let videoPlayer = AVPlayer(url: videoUrl)
+
+      // To make the video loop
+      videoPlayer.actionAtItemEnd = .none
+      NotificationCenter.default.addObserver(
+          self,
+          selector: #selector(ViewController.playerItemDidReachEnd),
+          name: NSNotification.Name.AVPlayerItemDidPlayToEndTime,
+          object: videoPlayer.currentItem)
+
+      // Create the SpriteKit video node, containing the video player
+      let videoSpriteKitNode = SKVideoNode(avPlayer: videoPlayer)
+      videoSpriteKitNode.position = CGPoint(x: spriteKitScene.size.width / 2.0, y: spriteKitScene.size.height / 2.0)
+      videoSpriteKitNode.size = spriteKitScene.size
+      videoSpriteKitNode.yScale = -1.0
+      videoSpriteKitNode.play()
+      spriteKitScene.addChild(videoSpriteKitNode)
+
+      // Create the SceneKit scene
+      sceneView.isPlaying = true
+      ball.firstMaterial?.diffuse.contents = spriteKitScene
+      sceneView.pointOfView?.addChildNode(ballNode)
     }
+  
+  @objc func playerItemDidReachEnd(notification: NSNotification) {
+         if let playerItem: AVPlayerItem = notification.object as? AVPlayerItem {
+             playerItem.seek(to: kCMTimeZero)
+         }
+     }
   
   @objc func record() {
     
   }
-    
-    override func viewWillAppear(_ animated: Bool) {
+  var isWriting: Bool = false
+  @IBAction func onRecord(_ sender: UIButton) {
+      let recorder = RPScreenRecorder.shared()
+
+      if recorder.isRecording {
+          sender.backgroundColor = .lightGray
+    sender.titleLabel?.text = "Start record"
+          recorder.stopRecording { (preview, error) in
+              if let unwrappedPreview = preview {
+                  unwrappedPreview.previewControllerDelegate = self
+                  self.present(unwrappedPreview, animated: true)
+              }
+          }
+      } else {
+          recorder.startRecording { error in
+              if let unwrappedError = error {
+                  print(unwrappedError.localizedDescription)
+              } else {
+                  sender.backgroundColor = .red
+        sender.titleLabel?.text = "Stop record"
+                  print("record is started")
+              }
+          }
+      }
+//    if isWriting {
+//
+//      self.recorder?.finishWriting().onSuccess { [weak self] url in
+//        print("Recording Finished", url)
+//        DispatchQueue.global(qos: .background).async {
+//            if let urlData = NSData(contentsOf: url) {
+//                let documentsPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0];
+//                let filePath="\(documentsPath)/tempFile.mp4"
+//                DispatchQueue.main.async {
+//                    urlData.write(toFile: filePath, atomically: true)
+//                    PHPhotoLibrary.shared().performChanges({
+//                        PHAssetChangeRequest.creationRequestForAssetFromVideo(atFileURL: URL(fileURLWithPath: filePath))
+//                    }) { completed, error in
+//                        if completed {
+//                            print("Video is saved!")
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//
+//      }
+//
+//    }else {
+//      self.recorder?.startWriting().onSuccess {
+//             print("Recording Started")
+//           }
+//    }
+  }
+  override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
     }
@@ -142,4 +246,10 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 //
 //        foundGrid.update(anchor: planeAnchor)
     }
+}
+
+extension ViewController: RPPreviewViewControllerDelegate {
+  func previewControllerDidFinish(_ previewController: RPPreviewViewController) {
+    previewController.dismiss(animated: true, completion: nil)
+  }
 }
